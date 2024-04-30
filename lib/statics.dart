92fcc3e1eddbb281e01c1698ba55f8b31a9b3a0e
Trinistar <trinistar@gmx.de>
2024-04-30 08:02:17 +0200

    add url links in statics file

---
+++ lib/statics.dart
@@ -0,0 +1,43 @@
+const String chorusWebsite = 'https://bw-saengerbund.de/mitteilungen/ich-kann-nicht-singen-chor-stuttgart.html';
+const String laboratoriumWebsite = 'https://laboratorium-stuttgart.de/programm';
+const String musikwerkWebsite = 'https://musikwerk-stuttgart.de/offener-chor';
+const String kneipchorWebsite = 'https://www.facebook.com/people/Stuttgarter-Kneipenchor/100057080304413';
+const String femaleFellowsMail = 'mailto:info@femalefellows.com';
+const String baerenschlossWebsite = 'https://www.baerenschloessle-stuttgart.de/en';
+const String birkenkopfWebsite = 'https://www.stuttgart-tourist.de/a-birkenkopf-monte-scherbelino-stuttgart';
+const String fernsehturmWebsite = 'https://www.fernsehturm-stuttgart.de';
+const String waldauWebsite = 'https://www.stuttgart.de/leben/sport/sportstaetten/waldau.php';
+const String kraehenwaldWebsite = 'https://www.google.com/maps/place/Gau%C3%9Fstra%C3%9Fe,+70193+Stuttgart/@48.778323,9.0733987,12z/data=!4m5!3m4!1s0x4799db729a22bddd:0xd740a320bbf73df2!8m2!3d48.7783195!4d9.1434365';
+const String bismarkturmWebsite = 'https://www.stuttgart-tourist.de/a-bismarckturm-stuttgart';
+const String killesberparkWebsite = 'https://www.stuttgart-tourist.de/a-hoehenpark-killesberg';
+const String kriegsbergturmWebsite = 'https://www.stuttgart-tourist.de/a-kriegsbergturm-stuttgart';
+const String chinesischegartenWebsite = 'http://www.chinagarten-stuttgart.de/index.php?article_id=18';
+const String zackeWebsite = 'https://www.stuttgart-tourist.de/a-zahnradbahn-stuttgart';
+const String ausblickeWebsite = 'https://www.mein-erlebnis.blog/top-10-aussichtspunkte-in-den-weinbergen';
+const String siebenmuehlentalWebsite = 'https://www.leinfelden-echterdingen.de/Startseite/Stadt/Siebenmuehlental.html';
+const String grabkapelleWebsite = 'https://www.grabkapelle-rotenberg.de/start';
+const String solitudeWebsite = 'https://www.schloss-solitude.de';
+const String besigheimWebsite = 'https://www.besigheim.de/start/gaeste/sehenswuerdigkeiten.html';
+const String kapellaWebsite = 'https://www.rottenburg.de/wurmlinger+kapelle.51309.htm';
+const String badurachWebsite = 'https://www.badurach-tourismus.de/attraktion/uracher-wasserfall-bd0513a23d#/article/1b853b65-76aa-4f1e-ab57-6078712b13ad';
+const String alphabetisierungDownload = 'https://www.klett-sprachen.de/kostenlose-materialien/fuer-erwachsene/alphabetisierung/c-1347';
+const String goetheWebsite = 'https://www.goethe.de/prj/dfd/de/home.cfm';
+const String youtubedeutsch = 'https://www.youtube.com/channel/UC5ZnpdkQIit8TWhGVDiDnQQ';
+const String deutschtrainerWebsite = 'https://www.dw.com/de/deutsch-lernen/audiotrainer/s-3657';
+const String deutschenglischWebsite = 'https://www.bbc.co.uk/languages/german/guide/phrases.shtml';
+const String goetheappWebsite = 'https://www.goethe.de/de/spr/ueb.html';
+const String stadtderwoerterWebsite = 'https://www.goethe.de/de/spr/ueb/led.html';
+const String kennenlernenWebsite = 'https://www.goethe.de/de/spr/ueb/dkl.html';
+const String meinwegWebsite = 'https://www.goethe.de/de/spr/ueb.html';
+const String arbeitsplatzgoetheWebsite = 'https://www.goethe.de/de/spr/ueb/daa/all.html';
+const String cinemastuttgartWebsite = 'https://www.innenstadtkinos.de';
+const String cinemaxXWebsite = 'https://www.cinemaxx.de/kinoprogramm/stuttgart-liederhalle';
+const String sneakcinemaWebsite = 'https://www.sneak-kino.de';
+const String paradieslichtspieleWebsite = 'https://www.kino-bietigheim.de';
+
+
+
+
+
+  
+         
